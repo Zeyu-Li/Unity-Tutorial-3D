938d2e72853fc8cba7aa6e1b6711597ad02d6825
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-04-30 09:54:51 -0600

    finished movement

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float movementSpeed = 14f;
+
+    // gravity
+    public float gravity = -10f;
+    Vector3 velocity;
+
+    // ground check
+    public Transform groundCheck;
+    public float checkRadius = 0.1f;
+    public LayerMask ground;
+
+    public bool isGrounded = false;
+
+    // jump
+    public float jump = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // is grounded calculations
+        isGrounded = Physics.CheckSphere(groundCheck.position, checkRadius, ground);
+
+        if (isGrounded) {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move*movementSpeed*Time.deltaTime);
+
+        // jump
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jump * -2f * gravity);
+        }
+
+        velocity.y += gravity*Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
